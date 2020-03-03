97aa954a9a017ba439594e4368dcaf4655de0aa4
Walter Xie <walterxie@users.noreply.github.com>
2020-03-03 16:12:42 +1300

    shuffle probs

--- src/test/beast/util/BinarySearchSamplingTest.java
+++ src/test/beast/util/BinarySearchSamplingTest.java
@@ -2,9 +2,13 @@ package test.beast.util;
 
 import beast.util.RandomUtils;
 import beast.util.Randomizer;
+import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 import static org.junit.Assert.assertArrayEquals;
 
@@ -12,24 +16,39 @@ import static org.junit.Assert.assertArrayEquals;
  * @author Walter Xie
  */
 public class BinarySearchSamplingTest {
+    final int nrOfStates = 60;
+    final int ite = 100000000; // 100 million
+
+    double[] prob;
+    double[] cpd;
+    @Before
+    public void setUp() throws Exception {
+        // unnormalized probabilities
+        List<Double> list = new ArrayList<>();
+        for (int i = 0; i < nrOfStates; i++)
+            list.add((double) (i+1));
+
+        //*** shuffle ***
+        Collections.shuffle(list);
+        double[] pr = list.stream().mapToDouble(d -> d).toArray();
+
+        // normalized probabilities
+        prob = new double[nrOfStates];
+        // compute distribution
+        computeDistribution(pr, prob);
+        System.out.println("\nTrue probability distribution : " + Arrays.toString(prob) + "\n");
+
+        // unnormalized cumulative probabilities
+        cpd = new double[nrOfStates];
+        cpd[0] = pr[0];
+        for (int i = 1; i < pr.length; i++) {
+            cpd[i] = cpd[i-1] + pr[i];
+        }
+
+    }
 
     @Test
     public void binarySearch() {
-        int nrOfStates = 60;
-        // unnormalized probabilities
-        double[] pr = new double[nrOfStates];
-        // normalized probabilities
-        double[] prob = new double[nrOfStates];
-        double sum = 0;
-        for (int i = 0; i < pr.length; i++) {
-            pr[i] = i+1;
-            sum += pr[i];
-        }
-        for (int i = 0; i < pr.length; i++)
-            prob[i] = pr[i] / sum;
-        System.out.println("Normalized probability distribution : " + Arrays.toString(prob) + "\n");
-
-        int ite = 100000000; // 100 million
         //++++++  Linear time sampling ++++++//
         int w;
         int[] freq1 = new int[nrOfStates];
@@ -42,24 +61,13 @@ public class BinarySearchSamplingTest {
         System.out.println("Linear sampling time : " + timeLinear + " milliseconds.");
         System.out.println("Freq : " + Arrays.toString(freq1) + "\n");
 
-        sum = 0;
         double[] prob1 = new double[nrOfStates];
-        for (int i = 0; i < freq1.length; i++)
-            sum += freq1[i];
-        for (int i = 0; i < freq1.length; i++)
-            prob1[i] = freq1[i] / sum;
+        computeDistribution(freq1, prob1);
         System.out.println("Normalized probability : " + Arrays.toString(prob1) + "\n");
 
         assertArrayEquals(prob, prob1, 1E-4);
 
         //++++++  Binary search sampling ++++++//
-        // unnormalized cumulative probabilities
-        double[] cpd = new double[nrOfStates];
-        cpd[0] = pr[0];
-        for (int i = 1; i < pr.length; i++) {
-            cpd[i] = cpd[i-1] + pr[i];
-        }
-
         double random;
         int[] freq2 = new int[nrOfStates];
         start = System.currentTimeMillis();
@@ -72,15 +80,27 @@ public class BinarySearchSamplingTest {
         System.out.println("Binary search sampling time : " + timeBiSearch + " milliseconds.");
         System.out.println("Freq : " + Arrays.toString(freq2) + "\n");
 
-        sum = 0;
         double[] prob2 = new double[nrOfStates];
-        for (int i = 0; i < freq2.length; i++)
-            sum += freq2[i];
-        for (int i = 0; i < freq2.length; i++)
-            prob2[i] = freq2[i] / sum;
+        computeDistribution(freq2, prob2);
         System.out.println("Normalized probability : " + Arrays.toString(prob2) + "\n");
 
         assertArrayEquals(prob, prob2, 1E-4);
+    }
+
+    private void computeDistribution(double[] pr, double[] distr) {
+        double sum = 0;
+        for (int i = 0; i < pr.length; i++)
+            sum += pr[i];
+        for (int i = 0; i < pr.length; i++)
+            distr[i] = pr[i] / sum;
+    }
+
+    private void computeDistribution(int[] freq, double[] distr) {
+        double sum = 0;
+        for (int i = 0; i < freq.length; i++)
+            sum += freq[i];
+        for (int i = 0; i < freq.length; i++)
+            distr[i] = freq[i] / sum;
     }
 
 
