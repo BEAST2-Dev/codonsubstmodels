f46ff65c4990a8f0e28c1015206fea31f3968800
Walter <walterxie@users.noreply.github.com>
2022-11-15 16:44:24 +1300

    Find how to load Codon data type in test #8

--- test/codonmodels/evolution/alignment/CodonAlignmentTest.java
+++ test/codonmodels/evolution/alignment/CodonAlignmentTest.java
@@ -4,10 +4,12 @@ import beast.base.evolution.alignment.Alignment;
 import beast.base.evolution.alignment.Sequence;
 import beast.base.evolution.datatype.DataType;
 import beast.pkgmgmt.BEASTClassLoader;
+import beast.pkgmgmt.PackageManager;
 import codonmodels.evolution.CodonData;
 import codonmodels.evolution.datatype.Codon;
 import org.junit.Test;
 
+import java.io.File;
 import java.util.List;
 import java.util.Set;
 
@@ -37,10 +39,16 @@ public class CodonAlignmentTest {
 
     @Test
     public void testGettingDataType() {
-        //TODO
+        String PROJECT_DIR = System.getProperty("user.dir");
+        System.out.println("user.dir = " + PROJECT_DIR);
+        File versionXML = new File(PROJECT_DIR + File.separator + "version.xml");
+        if (!versionXML.exists())
+            throw new IllegalArgumentException("Cannot locate codonsubstmodels/version.xml !");
+        BEASTClassLoader.initServices();
+        BEASTClassLoader.addServices(versionXML.getAbsolutePath());
         Set<String> dataTypes = BEASTClassLoader.loadService(DataType.class);
         System.out.println(dataTypes);
-        assertTrue("Cannot load Codon data type !", dataTypes.contains(Codon.CODON));
+        assertTrue("Cannot load Codon data type !", dataTypes.contains(Codon.class.getName()));
     }
 
     @Test
